c71c3909027860c1f2edcd093976a3712a946458
feel <fosiuslegend@gmail.com>
2024-02-20 16:36:13 +0300

    issue#1 & issue#5

--- Domain/DTOS/CocktailDTO.cs
+++ Domain/DTOS/CocktailDTO.cs
@@ -5,6 +5,7 @@ namespace Domain.DTOS
     public class CocktailDTO : EntityBase
     {
         public string? Name { get; set; }
+        public string? Description { get; set; }
         public string? AuthorUsername { get; set; }
         public List<int> IngredientsId { get; set; } = new();
     }
